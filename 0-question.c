042abb9ac4c8fabfb12df90bb902512c23dda171
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-06-25 13:10:48 +0100

    First implementation

--- 0-question.c
+++ 0-question.c
@@ -1,12 +1,17 @@
 #include "main.h"
 /**
  * _printf - a function that prints string to the output
- * @format: the output of the string 
+ * @format: the output of the string
+ * Return: the value of the string
  */
 int _printf(const char *format, ...)
 {
 	int count;
 	va_list ap;
+
+	if (format == NULL)
+		return (-1);
+
 	va_start(ap, format);
 
 	count = vprintf(format, ap);
