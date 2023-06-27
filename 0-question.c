a9e71d17a6cf5d5b2ed28ee5b44723cefd581fde
khaoularj <khaoula.raja1@gmail.com>
2023-06-27 10:52:45 -0700

    explanation of code

--- 0-question.c
+++ 0-question.c
@@ -3,6 +3,10 @@
  * _printf - a function that prints string to the output
  * @format: the output of the string
  * Return: the value of the string
+ * Explanation; the _printf funtion holds a variable number of arguments
+ * first local variable(count) will hold the number of characters printed
+ * second local variable(list) is used for traversing the var arguments list
+ * the vprintf function prints the formatted output to standard output
  */
 int _printf(const char *format, ...)
 {
